039884a3e66e4fd923db46703daa69c8756695ac
hodeiez <hodeinaiz@hotmail.com>
2021-01-14 10:36:06 +0100

    findWorkers working except with last element, need to fix logic

--- src/KotlinUsingJava.kt
+++ src/KotlinUsingJava.kt
@@ -23,21 +23,42 @@ fun findSuperior(name:String):List<String>{
     fun buildList(name:String):List<String>{
         if(name.isNotBlank()){
             result.add(name)
-            println(result)
-            buildList(createList().filter{e->e.child.contains(name)}.map(Arbetare::getName).last())}
-
+          try{
+              buildList(createList().filter{it.child.contains(name)}.map(Arbetare::getName).last())
+        } catch(e :NoSuchElementException){
+              return result.reversed()
+          }
+        }
         return result
-
     }
-
     return if(name==createList()[0].name){
         result.add(createList()[0].name)
         result }
     else{
-        buildList( createList().filter{e->e.child.contains(name)}.map(Arbetare::getName).last())
+        buildList( createList().filter{it.child.contains(name)}.map(Arbetare::getName).last())
     }
 }
+fun findWorkers(name:String):List<String>{
+    val result=mutableListOf<String>()
+    fun buildList(name:String):List<String>{
+        if(name.isNotBlank()&& !result.contains(createList()[createList().size-1].name)){
+          //change replace to regex!!!!
+            name.replace("[","").replace("]","").split( ", ").forEach{result.add(it)}
+            buildList(createList().filter{result.contains(it.name)}.map(Arbetare::getChild).flatten().toString())
+        }
+        else
+            result.add( name.replace("[","").replace("]",""))
+        return result
+    }
+    return if (name==createList()[createList().size-1].name)
+    result
+    else
+        buildList(createList().filter{it.name==name}.map(Arbetare::getChild).flatten().toString())
+
+}
 fun main() {
-findSuperior("Räven").forEach(::println)
+//findSuperior("Räven").forEach(::println)
+findWorkers("Räven").forEach(::println)
+findWorkers("Skumtomten").forEach(::println)
 
 }
