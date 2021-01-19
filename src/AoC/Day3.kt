5b44015fab60801ab936b383b4790fb5f6a859f9
hodeiez <hodeinaiz@hotmail.com>
2021-01-20 00:29:16 +0100

    playing to AoC, day3 hard coded puzzle2

--- src/AoC/Day3.kt
+++ src/AoC/Day3.kt
@@ -9,21 +9,34 @@ import java.io.File
  * Project: kotlinTest
  * Copyright: MIT
  */
-fun main(){
+fun main() {
     //done in an awful way hard coded the pattern repetition and used a nasty for loop
-    val input= File("src/AoC/day3.txt")
-    val theList=input.readLines().map{it}
+    val input = File("src/AoC/day3.txt")
+    val theList = input.readLines().map { it }
 
-    val mList= mutableListOf<Char>()
-    var j=0
-    for(i in theList){
-        mList.add(i.toCharArray()[j])
-        j+=3
-        if(j>=i.length) j -= i.length
-   }
-   println( mList.filter{ it == '#' }.count())
-    fun slopeCheck(jump:Int,toRight:Int){
+    val mList = mutableListOf<Char>()
+
+    fun findTrees(down: Int, right: Int) {
+        var rightAwful = right
+        for (i in down until theList.size step down) {
+            mList.add(theList[i].toCharArray()[rightAwful])
+            rightAwful += right
+            if (rightAwful >= theList[i].length) rightAwful -= theList[i].length
+        }
+    }
+    findTrees(2, 1)
+//78
+    //247
+    //68
+    //69
+    //78
+    val number:Long=90396072.toLong()*33
+    println(number)
+    println(mList.filter { it == '#' }.count().toLong())
+    fun slopeCheck(jump: Int, toRight: Int) {
 
     }
 
-}
+
+
+    }
