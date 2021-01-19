d05b5d9895792098418848325bc02529832c6825
hodeiez <hodeinaiz@hotmail.com>
2021-01-19 23:33:57 +0100

    playing to AoC, day3 got rid of the awful repeat method

--- src/AoC/Day3.kt
+++ src/AoC/Day3.kt
@@ -12,15 +12,18 @@ import java.io.File
 fun main(){
     //done in an awful way hard coded the pattern repetition and used a nasty for loop
     val input= File("src/AoC/day3.txt")
-    val theList=input.readLines().map{it.repeat(50)}
-    var j=0
-    val mList= mutableListOf<Char>()
+    val theList=input.readLines().map{it}
 
-   for(i in theList){
-       mList.add(i.toCharArray()[j])
-       j+=3
+    val mList= mutableListOf<Char>()
+    var j=0
+    for(i in theList){
+        mList.add(i.toCharArray()[j])
+        j+=3
+        if(j>=i.length) j -= i.length
    }
    println( mList.filter{ it == '#' }.count())
+    fun slopeCheck(jump:Int,toRight:Int){
 
+    }
 
 }
