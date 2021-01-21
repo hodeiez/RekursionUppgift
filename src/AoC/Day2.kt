93b7cc0cc418af3e4377e777a424511c9e7be779
hodeiez <hodeinaiz@hotmail.com>
2021-01-22 00:12:52 +0100

    AoC day 2 done, made a version without counter and if line, not so beautiful

--- src/AoC/Day2.kt
+++ src/AoC/Day2.kt
@@ -10,27 +10,30 @@ import java.io.File
  * Copyright: MIT
  */
 
-/*
-1-3 a: abcde
-1-3 b: cdefg
-2-9 c: ccccccccc
-old testin stuff
- // val input= File("src/AoC/day2.txt").readLines().map{it.toCharArray().filter{it.isLetterOrDigit()}}
-  //input.forEach { println(validCode(it[0].toString().toInt(),it[1].toString().toInt(),it[2],it.toMutableList().slice(3 until it.size).toString()))}
- */
+//ONE LINE SOLUTIONS
+fun validCode(min:Int, max:Int, letter: String, code:String)=code.toCharArray().filter{it.toString()==letter}.count().takeIf{ it in min..max }.let{it?.minus(it)?.plus(1)}?:0
 
+fun validCodePart2(min:Int, max:Int, letter: String, code:String)= code.toCharArray().takeIf{(it[min-1].toString()==letter || it[max-1].toString()==letter)&& it[min-1]!=it[max-1]}.let{it?.size?.minus(it.size)?.plus(1)}?:0
+
+//READABLE SOLUTIONS
+/*
 fun validCode(min:Int, max:Int, letter: String, code:String):Int{
     var count=0
     if(code.toCharArray().filter{it.toString()==letter}.count().takeIf{ it in min..max }!=null)
         count++
     return count
 }
+
+ */
+/*
 fun validCodePart2(min:Int, max:Int, letter: String, code:String):Int{
     var count=0
     if(code.toCharArray().takeIf{(it[min-1].toString()==letter || it[max-1].toString()==letter)&& it[min-1]!=it[max-1]}!=null)
         count++
     return count
 }
+
+ */
 
 fun main(){
 
